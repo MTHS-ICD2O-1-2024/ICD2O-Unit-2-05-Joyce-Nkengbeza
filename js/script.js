50667f878e3022cb95a8a265f1766c7cbe69cc17
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-24 16:59:40 +0000

    changed variables

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Joyce Nkengbeza
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+"use strict"
+
+function calculateAreaOfTriangle() {
+  // input
+  const baselength = parseInt(document.getElementById('base-length').value)
+  const heightlength = parseInt(document.getElementById('height-length').value)
+
+  // process
+    const areaOfTriangle = (baselength * heightlength) /2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
+
